69d193ee863de462e58de13ed3a119d01da74baa
AbdullahMukadam <abdullahmukadam21@gmail.com>
2024-08-01 20:59:58 +0530

    updated connectdb

--- Backend/dB/connectDb.js
+++ Backend/dB/connectDb.js
@@ -3,8 +3,9 @@ import mongoose from 'mongoose';
 const connectDb = async () => {
   try {
     const data = await mongoose.connect(
-      'mongodb+srv://abdullahmukadam21:zachmukaa@backendpractise.ouc5tlh.mongodb.net/'
+      "mongodb+srv://abdullahmukadam21:zachmukaa@backendpractise.ouc5tlh.mongodb.net/"
     );
+    console.log(data)
   } catch (error) {
     throw new error();
   }
